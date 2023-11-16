3122ef6bfa6f65249d246c28da1c2c81fb79026b
DanielsJuksis <daniels.juksis@grobinasskola.lv>
2023-11-16 14:53:14 +0200

    2.uzdevums

--- 2.uzdevums.py
+++ 2.uzdevums.py
@@ -1,6 +1,6 @@
 viens = 1
     # kamēr mainīgais VIENS kāpināts kvadrātā nav lielāks par 1000 pieskaitīt +1
-while viens**2 <= 1000:
+while viens**2 < 1000:
     viens += 1
 
 print(f"pirmais skaitlis kura kvadrāts ir lielāks par 1000, ir: {viens}")
